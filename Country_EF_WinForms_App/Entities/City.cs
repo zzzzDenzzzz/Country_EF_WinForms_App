8e357d6e410b160affad23fa4d3e4785b6e3a10d
zzzzDenzzzz <12den24@gmail.com>
2023-02-20 21:06:04 +0300

    add entities

---
+++ Country_EF_WinForms_App/Entities/City.cs
@@ -0,0 +1,17 @@
+﻿namespace Country_EF_WinForms_App.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Population { get; set; }
+
+        public bool IsCapital { get; set; }
+
+        public int CountryId { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+}
---
+++ Country_EF_WinForms_App/Entities/Country.cs
@@ -0,0 +1,21 @@
+﻿using Country_EF_WinForms_App.Constants;
+
+namespace Country_EF_WinForms_App.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Area { get; set; }
+
+        public decimal Population { get; set; }
+
+        public int CapitalId { get; set; }
+
+        public PartsOfTheWorld PartOfTheWorld { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
