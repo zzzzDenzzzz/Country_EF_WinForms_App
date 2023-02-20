24e75767ec4b0b388be839be1965f850414791a9
zzzzDenzzzz <12den24@gmail.com>
2023-02-20 21:28:25 +0300

    add entitiesMappings

---
+++ Country_EF_WinForms_App/EntityMappings/CityEntityMap.cs
@@ -0,0 +1,18 @@
+﻿using Country_EF_WinForms_App.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Country_EF_WinForms_App.EntityMappings
+{
+    internal class CityEntityMap : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.HasOne(e => e.Country)
+                .WithMany(x => x.Cities)
+                .HasForeignKey(e => e.Id)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
---
+++ Country_EF_WinForms_App/EntityMappings/CountryEntityMap.cs
@@ -0,0 +1,14 @@
+﻿using Country_EF_WinForms_App.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Country_EF_WinForms_App.EntityMappings
+{
+    public class CountryEntityMap : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(100);
+        }
+    }
+}
